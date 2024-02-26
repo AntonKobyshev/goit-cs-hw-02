c1af747edade2dec6e9d66c9c0a2dc93cc971e8b
Anton Kobyshev <antonkobyshev@gmail.com>
2024-02-26 22:53:17 +0100

    Update task_1.sh

--- task_1.sh
+++ task_1.sh
@@ -1,10 +1,20 @@
-urls=(https://google.com https://facebook.com https://twitter.com)
+#!/bin/bash
 
-for url in "${urls[@]}"; do
-    status_code=$(curl -L -o /dev/null -s -w "%{http_code}"  $url)
-    if [ "$status_code" -eq "200" ]; then
-        echo "$url is UP" >> website_status.log
-    else        
-        echo "$url is DOWN" >> website_status.log
+# Список вебсайтів для перевірки
+websites=("https://google.com" "https://facebook.com" "https://twitter.com")
+
+# Назва файлу логів
+log_file="website_status.log"
+
+# Перевірка доступності вебсайтів та запис результатів у файл логів
+for website in "${websites[@]}"; do
+    if curl -s -L --head --request GET -w '%{http_code}\n' "$website" | grep "200" > /dev/null; then
+        status="UP"
+    else
+        status="DOWN"
     fi
+    echo "$website is $status" >> "$log_file"
+    echo "$website is $status"
 done
+
+echo "Результати записані у файл логів: $log_file"
